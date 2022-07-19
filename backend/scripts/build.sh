48259aaead3378a23a37a69422c024fe96675096
Bruno Mariz <bruno.mariz@semantix.inc>
2022-07-19 12:42:59 -0300

    fix: kill container before prune

--- backend/scripts/build.sh
+++ backend/scripts/build.sh
@@ -1,5 +1,5 @@
-docker system prune
 docker kill lab_engsoft_backend
+docker system prune
 docker rmi -f lab_engsoft_backend
 docker build -t lab_engsoft_backend .
 docker run --rm -d --name lab_engsoft_backend -p 8080:80 -v ${PWD}:/code lab_engsoft_backend
