4fc95f9152bfea25d586802ef0ed4436bd330040
Rafael <rafaelguaracho@gmail.com>
2022-07-21 02:12:52 -0300

    versaook

--- backend/app/routes/usuario_router.py
+++ backend/app/routes/usuario_router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, HTTPException, Path, Depends
 from app.config import Sessionlocal
 from sqlalchemy.orm import Session
-from app.schemas import usuarioSchema, requestUsuarios, responseUsuarios
+from app.schemas import usuarioSchema, requestUsuario, responseUsuario
 from app.controllers.usuario_controller import usuarioController
 from app.services.usuario_service import usuarioService
 
@@ -23,21 +23,21 @@ async def get_usuarios():
     return usuario_controller.get_usuarios(Sessionlocal())
 
 @usuario_router.get("/usuarios")
-async def search_usuario(request:requestUsuarios):
+async def search_usuario(request:requestUsuario):
     return usuario_controller.get_usuarioEspecifico(Sessionlocal(),request)
 
 @usuario_router.post("/create")
-async def create_usuario(request:requestUsuarios):
+async def create_usuario(request:requestUsuario):
     return usuario_controller.create_usuarios(Sessionlocal(), request)
 
 @usuario_router.post("/remove")
-async def remove_usuario(request:requestUsuarios):
+async def remove_usuario(request:requestUsuario):
     return usuario_controller.remove_usuarios(Sessionlocal(), request)
 
 @usuario_router.post("/update")
-async def update_usuario(request:requestUsuarios):
+async def update_usuario(request:requestUsuario):
     return usuario_controller.update_usuarios(Sessionlocal(), request)
 
 @usuario_router.post("/login")
-async def verify_usuario(request:requestUsuarios):
+async def verify_usuario(request:requestUsuario):
     return usuario_controller.verify_usuario(Sessionlocal(), request)
