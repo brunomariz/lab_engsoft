69d4ba6524a5938859cbc90368d4f2954d53fc8e
Rafael <rafaelguaracho@gmail.com>
2022-07-20 21:25:50 -0300

    apenas getclient

--- backend/app/clienteCRUD.py
+++ backend/app/clienteCRUD.py
@@ -22,20 +22,20 @@ class ClientService(BaseService):
         resultado = db.query(cliente).filter(cliente.CPF_cliente == CPF_cliente).first()
         return pd.from_sql(resultado).to_dict('records')
 
-    def create_cliente(self, db:Session, Cliente:clienteSchema):
+    def createCliente(self, db:Session, Cliente:clienteSchema):
         _cliente = cliente(nome_cliente=Cliente.nome_cliente)
         db.add(_cliente)
         db.commit()
         db.refresh(_cliente)
         return pd.from_sql(_cliente).to_dict('records')
 
-    def remove_cliente(self,db:Session, CPF_cliente:str):
+    def removeCliente(self,db:Session, CPF_cliente:str):
         _cliente = self.getClienteEspecifico(db,CPF_cliente)
         db.delete(_cliente)
         db.commit()
         return pd.from_sql(_cliente).to_dict('records')
 
-    def update_cliente(self,db:Session, CPF_cliente:str, nome_cliente: str):
+    def updateCliente(self,db:Session, CPF_cliente:str, nome_cliente: str):
         _cliente= self.getClienteEspecifico(db, CPF_cliente)
         _cliente.nome_cliente = nome_cliente
         db.commit()
--- backend/app/controllers/client_controller.py
+++ backend/app/controllers/client_controller.py
@@ -9,13 +9,13 @@ class ClientController(BaseController):
     def __init__(self, service=...) -> None:
         super().__init__(ClientService())
 
-    def getClientes(self,db:Session, requestCliente):
-        return self.service.get_Clientes(db)
-    def get_clienteEspecifico(self,db:Session, requestCliente):
-        return self.service.get_ClienteEspecifico(db, requestCliente.parameter.CPF_cliente)
-    def create_cliente(self,db:Session, requestCliente):
-        return self.service.create_cliente(db, requestCliente.parameter)
-    def remove_cliente(self,db:Session, requestCliente):
-        return self.service.remove_cliente(db, requestCliente.parameter.CPF_cliente)
-    def update_cliente(self,db:Session, requestCliente):
-        return self.service.update_cliente(db, requestCliente.parameter.CPF_cliente, requestCliente.parameter.CPF_cliente)
+    def get_clientes(self,db:Session):
+        return self.service.getClientes(db)
+    # def get_clienteEspecifico(self,db:Session, requestCliente):
+    #     return self.service.getClienteEspecifico(db, requestCliente.parameter.CPF_cliente)
+    # def create_cliente(self,db:Session, requestCliente):
+    #     return self.service.createCliente(db, requestCliente.parameter)
+    # def remove_cliente(self,db:Session, requestCliente):
+    #     return self.service.removeCliente(db, requestCliente.parameter.CPF_cliente)
+    # def update_cliente(self,db:Session, requestCliente):
+    #     return self.service.updateCliente(db, requestCliente.parameter.CPF_cliente, requestCliente.parameter.CPF_cliente)
